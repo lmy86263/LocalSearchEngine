6ad78424f220e12dd80b62cddb684c6a109af6a9
李梦阳 <lmy86263@limengyangdeMacBook-Pro.local>
2018-08-22 20:34:02 +0800

    initial creation

---
+++ src/main/java/lmy86263/localsearchengine/LocalsearchengineApplication.java
@@ -0,0 +1,12 @@
+package lmy86263.localsearchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalsearchengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalsearchengineApplication.class, args);
+	}
+}
---
+++ src/test/java/lmy86263/localsearchengine/LocalsearchengineApplicationTests.java
@@ -0,0 +1,16 @@
+package lmy86263.localsearchengine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LocalsearchengineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
